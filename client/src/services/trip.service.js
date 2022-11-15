bdcca3db778a1172468c477aa1c30de2808ba4d4
Johnny Yu <qixingyu0707@gmail.com>
2022-11-14 23:58:28 -0800

    Set API URL

--- client/src/services/trip.service.js
+++ client/src/services/trip.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/trips";
+const API_URL = "https://uvic-carpool-project.herokuapp.com/api/trips";
 
 class TripService {
   post(title, description, price) {
