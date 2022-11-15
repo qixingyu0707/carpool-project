ee14c310ac7468757475e5cb0651db515224abfa
Johnny Yu <qixingyu0707@gmail.com>
2022-11-14 23:45:39 -0800

    Make changes for deployment2.1

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname), "client", "build", "index.html");
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
